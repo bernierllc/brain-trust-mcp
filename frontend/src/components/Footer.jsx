e9dee3edf6a19a13554ed1b110cd85c82d3abbc7
Matt Bernier <mkbernier@gmail.com>
2025-10-04 14:00:52 -0600

    Updated Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -54,6 +54,7 @@ function Footer() {
             MIT License - Open source and free to use
           </p>
           <p className="footer-copyright">
+            Built by <a href="https://mbernier.com">Matt Bernier</a> in Colorado <br />
             Built with <a href="https://cursor.com/" target="_blank">Cursor</a>, <a href="https://bolt.new/?rid=bmy46m" target="_blank">Bolt.new</a>, <a href="https://gofastmcp.com/getting-started/welcome" target="_blank">FastMCP</a>, <a href="https://chat.openai.com" target="_blank">OpenAi</a>, and <a href="https://react.dev">React</a>
           </p>
         </div>
